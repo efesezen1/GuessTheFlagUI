5380a7059b1878f025e033ea6b5e643bbab3d0f5
Efe Sezen <efesezen1@gmail.com>
2024-12-23 16:53:15 +0300

    End of the project.

--- GuessTheFlagUI/ContentView.swift
+++ GuessTheFlagUI/ContentView.swift
@@ -8,14 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var countries:[String] = ["Estonia","France","Germany","Ireland","Italy","Monaco","Nigeria","Poland","Spain","UK","Ukraine","US"].shuffled()
+    @State private var alertShown:Bool = false
+    @State private var score:Int = 0
+    @State private var result:Int = Int.random(in: 0..<3)
+    @State private var scoreTitle:String = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            LinearGradient(colors: [.red,.blue,.white], startPoint: .top, endPoint: .bottom).ignoresSafeArea()
+            VStack(spacing:30){
+                VStack{
+                Text("What is the flag of")
+                        .bold()
+                    Text(countries[result])
+                        .fontWeight(.semibold)
+                        .font(.title)
+                }.foregroundStyle(.white)
+
+                ForEach(0..<3, id: \.self){ number in
+                    Button{
+                        checkAnswer(number)
+                    } label:{
+                        Image(countries[number]).cornerRadius(10)
+                    }
+                    .alert(scoreTitle,isPresented: $alertShown){
+                        Button("Next Question"){nextQuestion()}
+                    } message: {Text("Your score is \(score)")}
+                }
+                Text("Your score is \(score)").bold()
+            }
+            
         }
-        .padding()
+    }
+    
+    func checkAnswer (_ number:Int){
+        if number == result {
+            scoreTitle = "Right"
+            score += 1
+        } else {
+            scoreTitle = "Wrong"
+            if score <= 0 {
+                score = 0
+            } else{
+                score -= 1
+            }
+        }
+        alertShown = true
+    }
+    
+    func nextQuestion (){
+        countries.shuffle()
+        result = Int.random(in:0..<3)
     }
 }
 
