a5f1d6d40af30359759a01596698adf3d34d06d8
Efe Sezen <efesezen1@gmail.com>
2024-12-23 14:40:27 +0300

    Initial Commit

---
+++ GuessTheFlagUI/GuessTheFlagUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagUIApp.swift
+//  GuessTheFlagUI
+//
+//  Created by Efe Sezen on 23.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GuessTheFlagUITests/GuessTheFlagUITests.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagUITests.swift
+//  GuessTheFlagUITests
+//
+//  Created by Efe Sezen on 23.12.2024.
+//
+
+import Testing
+@testable import GuessTheFlagUI
+
+struct GuessTheFlagUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
